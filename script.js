5058b331b8a1084a3ef636005f4aeef4d9305884
Darion Valdez <Darionvldz@gmail.com>
2024-01-17 18:47:27 +0100

    refactor code

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const calculatorFrame = document.querySelector('.calculator');
 
 const MAX_CHARACTER_LENGTH = 8;
 const MULTIPLE = 100000000;
+const NR_OF_COLUMNS = 4;
+const BUTTONS_PER_COLUMN = 4;
 
 let firstOperand = 0;
 let secondOperand = 0;
@@ -22,112 +24,12 @@ let dividedByZero = false;
 //used in case the user keeps pressing the = button and then presses an operator button.
 let hasEvaluatedResult = false;
 
+//The numpad consists of two rows, the top row has 3 buttons - clear, delete and divide.
+//The bottom row consists of 4 columns.
+//They are ordered that way because it made it easier placing the long buttons.
+const topRowLayout = ['clear', 'delete', 'divide'];
 const buttonLayout = [7,4,1,0,8,5,2,'decimal',9,6,3,'equals','multiply','subtract','add'];
-
-for(let i = 0; i < 2; i++) {
-    let newRow = document.createElement('div');
-    newRow.classList.add('container', 'row');
-
-    if(i === 0) {
-        for(let i = 0; i < 3; i++){
-            let newDiv = document.createElement('div');
-            newDiv.classList.add('container', 'digit');
-            switch (i) {
-                case 0: 
-                        newDiv.classList.add('long-horizontal');
-                        newDiv.textContent = 'CLEAR'
-                        newDiv.addEventListener('click', reset);
-                        break;
-                case 1: 
-                        newDiv.textContent = '◄'
-                        newDiv.addEventListener('click', backspace);
-                        break;
-                case 2: 
-                        newDiv.classList.add('shift-1', 'scale-1', 'operator');
-                        newDiv.textContent = '÷'
-                        newDiv.addEventListener('click', ()=>{
-                            pressOperatorButton('divide');
-                        });
-                        break;
-                default: console.log('fel');
-            }
-            newRow.appendChild(newDiv);
-        }
-    } else {
-    let buttonIndex = 0;
-    for(let i = 0; i < 4; i++){
-        
-        
-        const newColumn = document.createElement('div');
-        newColumn.classList.add('container', 'column');
-            for(let i = 0; i < 4; i++){
-                console.log(buttonLayout[buttonIndex]);
-                let newDiv = document.createElement('div');
-                newDiv.classList.add('container', 'digit');
-
-                if(buttonLayout[buttonIndex] === 'decimal') {
-                    newDiv.addEventListener('click', addDecimal);
-                    newDiv.textContent = '.';
-                    newColumn.appendChild(newDiv);
-                    buttonIndex ++;
-                    continue;
-                }
-
-                if(buttonLayout[buttonIndex] === 'equals') {
-                    newDiv.addEventListener('click', pressEqualButton);
-                    newDiv.textContent = '=';
-                    newColumn.appendChild(newDiv);
-                    buttonIndex ++;
-                    continue;
-                }
-
-                if(buttonLayout[buttonIndex] === 'multiply') {
-                    newDiv.classList.add('operator');
-                    newDiv.addEventListener('click', ()=>{
-                        pressOperatorButton('multiply')
-                    });
-                    newDiv.textContent = 'X';
-                    newColumn.appendChild(newDiv);
-                    buttonIndex ++;
-                    continue;
-                }
-                
-                if(buttonLayout[buttonIndex] === 'subtract') {
-                    newDiv.classList.add('operator');
-                    newDiv.addEventListener('click', ()=>{
-                        pressOperatorButton('subtract')
-                    });
-                    newDiv.textContent = '-';
-                    newColumn.appendChild(newDiv);
-                    buttonIndex ++;
-                    continue;
-                }
-
-                if(buttonLayout[buttonIndex] === 'add') {
-                    newDiv.classList.add('operator', 'long');
-                    newDiv.addEventListener('click', ()=>{
-                        pressOperatorButton('add')
-                    });
-                    newDiv.textContent = '+';
-                    newColumn.appendChild(newDiv);
-                    buttonIndex ++;
-                    break;
-                }
-
-                newDiv.textContent = buttonLayout[buttonIndex];
-                const input = buttonLayout[buttonIndex];
-                newDiv.addEventListener('click',()=>{
-                    renderInput(input);
-                    setSecondOperand();
-                });
-                buttonIndex ++;
-                newColumn.appendChild(newDiv);
-            }
-            newRow.appendChild(newColumn);
-    }   
-}
-    numpad.appendChild(newRow);
-}
+let buttonIndex = 0;
 
 document.addEventListener('keydown',(e)=>{
     if (!isNaN(Number(e.key))) {
@@ -144,7 +46,105 @@ document.addEventListener('keydown',(e)=>{
     if(e.key === 'Enter' || e.key === '=') pressEqualButton();
 })
 
-//-------FUNCTIONS-----------------------//
+
+function addTopRow(){
+    let newRow = document.createElement('div');
+    newRow.classList.add('container', 'row');
+
+    topRowLayout.forEach((e)=> {
+        let newButton = document.createElement('div');
+        newButton.classList.add('container', 'digit');
+        switch (e) {
+            case 'clear': 
+                    newButton.classList.add('long-horizontal');
+                    newButton.textContent = 'CLEAR'
+                    newButton.addEventListener('click', reset);
+                    break;
+            case 'delete': 
+                    newButton.textContent = '◄'
+                    newButton.addEventListener('click', backspace);
+                    break;
+            case 'divide': 
+                    newButton.classList.add('shift-1', 'scale-1', 'operator');
+                    newButton.textContent = '÷'
+                    newButton.addEventListener('click', ()=>{
+                        pressOperatorButton('divide');
+                    });
+                    break;
+        }
+        newRow.appendChild(newButton);
+    })
+    numpad.appendChild(newRow);
+}
+
+function addButtons(){
+    let newRow = document.createElement('div');
+    newRow.classList.add('container', 'row');
+
+    for(let i = 0; i < NR_OF_COLUMNS; i++){
+        
+        let newColumn = document.createElement('div');
+        newColumn.classList.add('container', 'column');
+
+            for(let i = 0; i < BUTTONS_PER_COLUMN; i++){
+
+                let newButton = document.createElement('div');
+                newButton.classList.add('container', 'digit');
+
+                switch(buttonLayout[buttonIndex]){
+
+                    case 'decimal': newButton.addEventListener('click', addDecimal);
+                                    newButton.textContent = '.';
+                                    newColumn.appendChild(newButton);
+                                    buttonIndex++;
+                                    continue;
+
+                    case 'equals':  newButton.addEventListener('click', pressEqualButton);
+                                    newButton.textContent = '=';
+                                    newColumn.appendChild(newButton);
+                                    buttonIndex++;
+                                    continue;
+
+                    case 'multiply': newButton.classList.add('operator');
+                                    newButton.addEventListener('click', ()=>{ pressOperatorButton('multiply') });
+                                    newButton.textContent = 'X';
+                                    newColumn.appendChild(newButton);
+                                    buttonIndex++;
+                                    continue;
+                
+                    case 'subtract': newButton.classList.add('operator');
+                                    newButton.addEventListener('click', ()=>{ pressOperatorButton('subtract')});
+                                    newButton.textContent = '-';
+                                    newColumn.appendChild(newButton);
+                                    buttonIndex++;
+                                    continue;
+                    
+                    case 'add':     newButton.classList.add('operator', 'long');
+                                    newButton.addEventListener('click', ()=>{ pressOperatorButton('add') });
+                                    newButton.textContent = '+';
+                                    newColumn.appendChild(newButton);
+                                    buttonIndex++;
+                                    continue;
+
+                    case  undefined:      continue;
+                }
+        
+                newButton.textContent = buttonLayout[buttonIndex];
+                const input = buttonLayout[buttonIndex];
+
+                newButton.addEventListener('click',()=>{
+                    renderInput(input);
+                    setSecondOperand();
+                });
+
+                newColumn.appendChild(newButton);
+                buttonIndex++;
+            }
+        newRow.appendChild(newColumn);
+    }   
+    numpad.appendChild(newRow);
+}
+
 function operate(){
     switch(selectedOperator){
         case 'add': add();
@@ -237,7 +237,6 @@ function pressEqualButton(){
     hasEvaluatedResult = true;
 }
 function renderInput(input){
-    console.log(input);
     flashLCD();
     if(newLine || lcdText.textContent == '0' || dividedByZero) clearInput();
     if(lcdText.textContent.length < MAX_CHARACTER_LENGTH ) lcdText.textContent += input;
@@ -304,4 +303,6 @@ function shake(){
     }, 5000);
 }
 
+addTopRow();
+addButtons();
 welcome();
